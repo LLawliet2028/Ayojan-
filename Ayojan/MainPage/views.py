dabad0fd5c1ce209ccc756d4873932d98b18052d
PushkarOM <pushkarchaturvedi42@gmail.com>
2024-09-20 22:28:19 +0530

    UI changes

--- Ayojan/MainPage/views.py
+++ Ayojan/MainPage/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 #below function loads the landingpage html
 
 def landing_view(request):
-    return render(request,"MainPage/landingpage.html")
+    image_list = [
+        '..\\media\\venue_images\\Ahluwalia_PLC_60.jpg',
+        '..\\media\\venue_images\\Arora-Gandhi_22.jpg',
+        '..\\media\\venue_images\\Bal_Baral_and_Majumdar_17.jpg',
+    ]
+    return render(request, 'Mainpage/landingpage.html', {'images': image_list})
 
