e90b4f76ccd7a23d4a74ad263d9a479d7056cd7e
PushkarOM <pushkarchaturvedi42@gmail.com>
2024-09-21 16:57:35 +0530

    changes to paths used for grabbing templates

--- MainPage/views.py
+++ MainPage/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render
 
 def landing_view(request):
     image_list = [
-        '..\\media\\venue_images\\Ahluwalia_PLC_60.jpg',
-        '..\\media\\venue_images\\Arora-Gandhi_22.jpg',
-        '..\\media\\venue_images\\Bal_Baral_and_Majumdar_17.jpg',
+        '../media/venue_images/Ahluwalia_PLC_60.jpg',
+        '..//media//venue_images/Arora-Gandhi_22.jpg',
+        '../media/venue_images/Bal_Baral_and_Majumdar_17.jpg',
     ]
     return render(request, 'Mainpage/landingpage.html', {'images': image_list})
 
