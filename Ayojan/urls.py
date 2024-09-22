115e3abc0e953cf221c1e0a4ebe3a2179a0c12f0
PushkarOM <pushkarchaturvedi42@gmail.com>
2024-09-22 22:22:43 +0530

    css issue

--- Ayojan/urls.py
+++ Ayojan/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('booking/',include('Booking.urls')),
     path('payment/',include('Payment.urls'))
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 from django.conf import settings
 from django.conf.urls.static import static
 
