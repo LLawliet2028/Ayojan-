71dac2e095ca635933d941931a76c1c1436ebe2f
PushkarOM <pushkarchaturvedi42@gmail.com>
2024-09-27 00:31:05 +0530

    completed the mainpage

--- MainPage/views.py
+++ MainPage/views.py
@@ -9,7 +9,12 @@ def landing_view(request):
         '../media/venue_images/Bal_Baral_and_Majumdar_15.jpg',
     ]
     logo = [
-        '../media/logo/ayojan-logo1.jpg'
+        '../media/logo/ayojan-logo1.png'
     ]
-    return render(request, 'MainPage/landingpage.html', {'images': image_list, 'logo' : logo})
+    pfps = [
+        '../media/pfps/image.png',
+        '../media/pfps/image1.png',
+        '../media/pfps/image2.png'
+    ]
+    return render(request, 'MainPage/landingpage.html', {'images': image_list, 'logo' : logo, 'pfps' : pfps})
 
