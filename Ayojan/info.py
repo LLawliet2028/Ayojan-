c34ac9a79094a3066919016ba99186048afb07b2
Ranjan Mishra <109550148+LLawliet2028@users.noreply.github.com>
2025-02-17 21:54:28 +0530

    Update info.py

--- Ayojan/info.py
+++ Ayojan/info.py
@@ -1,6 +1,6 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ayojan.platform@gmail.com'
-EMAIL_HOST_PASSWORD = 'bsui nczx kyng asqj' 
+EMAIL_HOST_USER = 'ranjanmishra2028@gmail.com'
+EMAIL_HOST_PASSWORD = 'Ayanokoji_kiyotaka' 
 EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
